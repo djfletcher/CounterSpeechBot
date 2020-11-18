2148e73f272de7f8ed7e267c8648f1d9721e6bed
Neil Lohana <nlohana@yelp.com>
2020-11-17 17:11:13 -0800

    Change to a single new line delimited file

--- get_data.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DATA_DIRECTORY = os.getcwd() + '/data/'
-
-dataset = []
-for filename in os.listdir(DATA_DIRECTORY):
-    with open(os.path.join(DATA_DIRECTORY, filename), 'r') as f:
-        dataset.append(f.read())
