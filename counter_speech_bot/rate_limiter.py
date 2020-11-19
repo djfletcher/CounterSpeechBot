e400000b5fc31329fc279872a779be1f643ec61e
Daniel Fletcher <dfletch@yelp.com>
2020-11-18 18:27:15 -0800

    Add lots more options to the realtime streamer and improve processing and formatting of output

--- counter_speech_bot/rate_limiter.py
+++ counter_speech_bot/rate_limiter.py
@@ -3,13 +3,16 @@ import time
 
 
 class RateLimiter:
+	""" Basic rate limiter, nothing fancy """
 
-	def __init__(self, max_calls_per_second):
+	def __init__(self, max_calls_per_second, padding_microseconds=0):
 		self.each_call_duration_seconds = 1 / max_calls_per_second
-		# self.max_calls_per_second = max_calls_per_second
 		self.last_call_time = datetime.datetime.fromtimestamp(0)
+		self.padding_microseconds = padding_microseconds
 
 	def wait(self):
 		next_call_time = self.last_call_time + datetime.timedelta(seconds=self.each_call_duration_seconds)
 		time.sleep(max(0, next_call_time.timestamp() - datetime.datetime.now().timestamp()))
-		self.last_call_time = datetime.datetime.now()
+		# If padding is needed, pretend that the last call was made n microseconds later than it actually was.
+		# This is useful so that we are slightly below the rate limit and don't hit it
+		self.last_call_time = datetime.datetime.now() + datetime.timedelta(microseconds=self.padding_microseconds)
--- counter_speech_bot/realtime_bot.py
+++ counter_speech_bot/realtime_bot.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import csv
 from collections import defaultdict
+from typing import NamedTuple
+from typing import Dict
 
 import googleapiclient
 from googleapiclient import discovery
@@ -16,16 +18,29 @@ TWITTER_CONSUMER_SECRET = 'TWITTER_CONSUMER_SECRET'
 TWITTER_BEARER_TOKEN = 'TWITTER_BEARER_TOKEN'
 
 
+class ToxicTweet(NamedTuple):
+    tweet: Dict[str, object]
+    analysis: Dict[str, object]
+
+
 class CounterSpeechBot:
 
     def __init__(self, args):
         self.get_api_keys()
-        self.tweet_limit = args.tweet_limit
+        self.total_tweet_limit = args.total_tweet_limit
+        self.toxic_tweet_limit = args.toxic_tweet_limit
         self.attributes = args.attributes
+        self.toxicity_threshold = args.toxicity_threshold
+        self.identity_attack_threshold = args.identity_attack_threshold
+        self.insult_threshold = args.insult_threshold
+        self.include_non_english = args.include_non_english
+
         self.service = discovery.build(
             'commentanalyzer', 'v1alpha1', developerKey=self.api_keys[PERSPECTIVE_API_KEY]
         )
         self.error_types_count = defaultdict(lambda: 0)
+        # This tracks the tweets (with analysis) that meet the thresholds passed in
+        self.toxic_tweets = []
 
     def get_api_keys(self):
         self.api_keys = {}
@@ -40,6 +55,7 @@ class CounterSpeechBot:
                 'text': tweet['data']['text'],
             },
             'requestedAttributes': {attribute: {} for attribute in self.attributes},
+            'spanAnnotations': True
         }
 
         try:
@@ -49,6 +65,7 @@ class CounterSpeechBot:
             e._get_reason()
             error_type = e.error_details[0]['errorType']
             self.error_types_count[error_type] += 1
+            print(self._format_tweet(tweet))
             print(f"- Skipping due to {error_type} error in Perspective API")
             return None
 
@@ -57,7 +74,9 @@ class CounterSpeechBot:
         Returns an iterator of approximately 1% of realtime tweets
         API Documentation: https://developer.twitter.com/en/docs/twitter-api/tweets/sampled-stream/api-reference/get-tweets-sample-stream
         """
-        url = 'https://api.twitter.com/2/tweets/sample/stream?tweet.fields=lang'
+        tweet_fields = ['lang', 'entities']
+        expansions = ['author_id']
+        url = f"https://api.twitter.com/2/tweets/sample/stream?tweet.fields={','.join(tweet_fields)}&expansions={','.join(expansions)}"
         headers = {'Authorization': f"Bearer {self.api_keys[TWITTER_BEARER_TOKEN]}"}
         print(f"Sampling 1% of realtime tweets...")
         response = requests.get(url, headers=headers, stream=True)
@@ -66,38 +85,91 @@ class CounterSpeechBot:
 
         return response.iter_lines(decode_unicode=True)
 
+    def get_attribute_score(self, analysis, attribute):
+        """ Helper to get the score of a particular attribute from the perspective API response """
+        return analysis['attributeScores'][attribute]['summaryScore']['value']
+
+    def meets_thresholds(self, analysis):
+
+        def is_above_threshold(analysis, attribute):
+            arg_name = f"{attribute.lower()}_threshold"
+            threshold = getattr(self, arg_name)
+            return self.get_attribute_score(analysis, attribute) > threshold
+
+        return all(is_above_threshold(analysis, attribute) for attribute in self.attributes)
+
+    def _format_tweet(self, tweet):
+        author = tweet['includes']['users'][0]['name']
+        text = tweet['data']['text']
+        return f"{author} -> {text}"
+
+    def _print_formatted_analysis(self, analysis, padding=''):
+        for attribute in self.attributes:
+            print(f"{padding}- {attribute}: {self.get_attribute_score(analysis, attribute)}")
+
     def main(self):
-        rate_limiter = RateLimiter(max_calls_per_second=1)
-        tweet_count = 0
+        rate_limiter = RateLimiter(max_calls_per_second=1, padding_microseconds=1000)
+        total_tweet_count = 0
         for tweet in self.sample_realtime_tweets():
-            if tweet_count >= self.tweet_limit:
+            if (
+                self.total_tweet_limit and total_tweet_count >= self.total_tweet_limit
+                or self.toxic_tweet_limit and len(self.toxic_tweets) >= self.toxic_tweet_limit
+            ):
                 break
 
-            tweet = json.loads(tweet)
-            if tweet['data']['lang'] != 'en':
+            try:
+                tweet = json.loads(tweet)
+            except json.decoder.JSONDecodeError as e:
+                self.error_types_count['JSONDecodeError'] += 1
+                print(f"- Skipping due to JSONDecodeError")
                 continue
-            rate_limiter.wait()
-            print(f"Tweet: {tweet}")
-            response = self.get_toxicity(tweet)
-            if response:
-                for attribute in self.attributes:
-                    print(f"- {attribute}: {response['attributeScores'][attribute]['summaryScore']['value']}")
-            tweet_count += 1
 
-        print('Done!')
-        print(f"Processed {tweet_count} tweets.")
-        print(f"Total errors encountered: {sum(self.error_types_count.values())}")
+            if tweet['data']['lang'] != 'en' and not self.include_non_english:
+                continue
+
+            print('------------------------------------------------')
+
+            rate_limiter.wait()
+            analysis = self.get_toxicity(tweet)
+            if analysis and self.meets_thresholds(analysis):
+                print("===================MEETS THRESHOLDS===================")
+                print(self._format_tweet(tweet))
+                print(self._print_formatted_analysis(analysis))
+                print("======================================================")
+                # keep track of these
+                self.toxic_tweets.append(ToxicTweet(tweet, analysis))
+            else:
+                print(self._format_tweet(tweet))
+
+            total_tweet_count += 1
+
+        print('\n\n\nSummary:')
+        print(f"- Processed {total_tweet_count} tweets")
+        print(f"- Identified {len(self.toxic_tweets)} toxic tweets:")
+        for idx, (tweet, analysis) in enumerate(self.toxic_tweets):
+            print('------------------------------------------------')
+            print(f"    {idx + 1} {self._format_tweet(tweet)}")
+            self._print_formatted_analysis(analysis, padding='      ')
+
+        print(f"- Encountered {sum(self.error_types_count.values())} errors:")
         for error_type, count in self.error_types_count.items():
-            print(f"- {len(count)} {error_type} errors")
+            print(f"    - {count} - {error_type}")
 
 
 if __name__ == '__main__':
     """
     Example command:
-    python -m main --atributes TOXICITY IDENTITY_ATTACK INSULT
+    python -m main --atributes TOXICITY IDENTITY_ATTACK INSULT --toxicity-threshold 0.75
     """
-    parser = argparse.ArgumentParser(description='Samples 1% of realtime tweets and scores them according to toxicity attributes from the Perspective API')
-    parser.add_argument('--tweet-limit', default=None, type=int, help='Quit when the specified number of tweets have been processed.')
+    parser = argparse.ArgumentParser(
+        description='Randomly samples roughly 1% of publicly available tweets in real-time and scores them according to '
+                    'toxicity attributes from the Perspective API'
+    )
+    parser.add_argument('--total-tweet-limit', default=None, type=int, help='Quit when the specified number of tweets have been processed.')
+    parser.add_argument('--toxic-tweet-limit', default=None, type=int, help='Quit when the specified number of toxic tweets have been processed.')
+    parser.add_argument('--toxicity-threshold', default=0.0, type=float, help='Only process tweets whose toxicity is scored above this threshold')
+    parser.add_argument('--identity-attack-threshold', default=0.0, type=float, help='Only process tweets whose identity attack is scored above this threshold')
+    parser.add_argument('--insult-threshold', default=0.0, type=float, help='Only process tweets whose insult is scored above this threshold')
     parser.add_argument(
         '--attributes',
         nargs='+',
@@ -105,5 +177,6 @@ if __name__ == '__main__':
         help='List of attributes to analyze for each tweet. See https://support.perspectiveapi.com/s/about-the-api-attributes-and-languages\n'
              'default: TOXICITY IDENTITY_ATTACK',
     )
+    parser.add_argument('--include-non-english', default=False, type=bool, help='If true, includes tweets in all languages. By default, only english tweets are processed.')
     args = parser.parse_args()
     CounterSpeechBot(args).main()
